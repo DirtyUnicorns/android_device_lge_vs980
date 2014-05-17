fc62a6ae4112778622f71e5af3b5c47fc5c336fa
AdityaSureka <adityasureka92@gmail.com>
2014-05-17 01:41:23 -0400

    We are DU
    Change-Id: Id8d70ba603e7f995f244598c387c11ca1aa7e02b

--- du.mk
+++ du.mk
@@ -14,19 +14,19 @@
 # limitations under the License.
 
 # Inherit APNs list
-$(call inherit-product, vendor/omni/config/cdma.mk)
+$(call inherit-product, vendor/du/config/cdma.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/du/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/vs980/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_vs980
+PRODUCT_NAME := du_vs980
 PRODUCT_DEVICE := vs980
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := lge
